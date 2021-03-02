95c601662781d79b97399aeaf10c00de24e65662
craigmcc <craigmcc@gmail.com>
2021-03-01 20:08:55 -0800

    More progress, but still issues with js-class template.

--- src/migrations/20210228-190154-create-volumes.js
+++
@@ -1,7 +0,0 @@
-exports.down = async (data, context) => {
-        console.info("CreateVolumes.down() not yet implemented");
-    }
-
-exports.up = async (data, context) => {
-        console.info("CreateVolumes.up() not yet implemented");
-    }
