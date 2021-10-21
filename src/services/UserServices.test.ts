cf2cef7b247b155817e7fa32290bee97a6ef0940
Craig McClanahan <craigmcc@gmail.com>
2021-10-21 10:38:00 -0700

    Flesh out remaining UserServices tests.

--- src/services/UserServices.test.ts
+++ src/services/UserServices.test.ts
@@ -163,15 +163,181 @@ describe("UserServices Functional Tests", () => {
     })
 
     describe("UserServices.exact()", () => {
-        // TODO
+
+        it("should fail on invalid username", async () => {
+
+            const INVALID_USERNAME = "abra cadabra";
+
+            try {
+                await UserServices.exact(INVALID_USERNAME);
+                expect.fail("Should have thrown NotFound");
+            } catch (error) {
+                if (error instanceof NotFound) {
+                    expect(error.message).to.include
+                    (`username: Missing User '${INVALID_USERNAME}'`);
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should pass on included children", async () => {
+
+            const OUTPUTS = await UserServices.all({
+                withAccessTokens: "",
+                withRefreshTokens: "",
+            });
+            OUTPUTS.forEach(async OUTPUT => {
+                const user = await UserServices.exact(OUTPUT.username);
+                expect(user.accessTokens).to.exist;
+                if (user.username === SeedData.USER_USERNAME_SUPERUSER) {
+                    expect(user.accessTokens.length).to.equal(SeedData.ACCESS_TOKENS_SUPERUSER.length);
+                } else {
+                    expect(user.accessTokens.length).to.equal(0);
+                }
+                expect(user.refreshTokens).to.exist;
+                if (user.username === SeedData.USER_USERNAME_SUPERUSER) {
+                    expect(user.refreshTokens.length).to.equal(SeedData.REFRESH_TOKENS_SUPERUSER.length);
+                } else {
+                    expect(user.refreshTokens.length).to.equal(0);
+                }
+            });
+
+        })
+
+        it("should pass on valid usernames", async () => {
+
+            const INPUTS = await UserServices.all();
+
+            INPUTS.forEach(async (INPUT) => {
+                const OUTPUT = await UserServices.exact(INPUT.username);
+                expect(OUTPUT.id).to.equal(INPUT.id);
+            })
+
+        })
+
     })
 
     describe("UserServices.find()", () => {
-        // TODO
+
+        it("should fail on invalid ID", async () => {
+
+            const INVALID_ID = -1;
+
+            try {
+                await UserServices.find(INVALID_ID);
+                expect.fail("Should have thrown NotFound");
+            } catch (error) {
+                if (error instanceof NotFound) {
+                    expect(error.message).to.include
+                    (`userId: Missing User ${INVALID_ID}`);
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should pass on included children", async () => {
+
+            const INPUTS = await UserServices.all({
+                withAccessTokens: "",
+                withRefreshTokens: "",
+            });
+
+            INPUTS.forEach(async INPUT => {
+                const OUTPUT = await UserServices.find(INPUT.id);
+                expect(OUTPUT.accessTokens).to.exist;
+                if (OUTPUT.username === SeedData.USER_USERNAME_SUPERUSER) {
+                    expect(OUTPUT.accessTokens.length).to.equal(SeedData.ACCESS_TOKENS_SUPERUSER.length);
+                } else {
+                    expect(OUTPUT.accessTokens.length).to.equal(0);
+                }
+                expect(OUTPUT.refreshTokens).to.exist;
+                if (OUTPUT.username === SeedData.USER_USERNAME_SUPERUSER) {
+                    expect(OUTPUT.refreshTokens.length).to.equal(SeedData.REFRESH_TOKENS_SUPERUSER.length);
+                } else {
+                    expect(OUTPUT.refreshTokens.length).to.equal(0);
+                }
+            })
+
+        })
+
+        it("should pass on valid IDs", async () => {
+
+            const INPUTS = await UserServices.all();
+
+            INPUTS.forEach(async (INPUT) => {
+                const OUTPUT = await UserServices.find(INPUT.id);
+                expect(OUTPUT.id).to.equal(INPUT.id);
+                expect(OUTPUT.password).to.equal("");
+            })
+
+        })
+
     })
 
     describe("UserServices.insert()", () => {
-        // TODO
+
+        it("should fail on duplicate username", async () => {
+
+            const EXISTING = await lookupUser(SeedData.USER_USERNAME_SUPERUSER);
+            const INPUT = {
+                name: "dummy",
+                password: "dummy",
+                scope: "dummy",
+                username: EXISTING.username,
+            };
+
+            try {
+                await UserServices.insert(INPUT);
+                expect.fail(`Should have thrown BadRequest`);
+            } catch (error) {
+                if (error instanceof BadRequest) {
+                    expect(error.message).to.include("is already in use");
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should fail on invalid input data", async () => {
+
+            const INPUT = {};
+
+            try {
+                await UserServices.insert(INPUT);
+                expect.fail(`Should have thrown BadRequest`);
+            } catch (error) {
+                if (error instanceof BadRequest) {
+                    expect(error.message).to.include("Is required");
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should pass on valid input data", async () => {
+
+            const INPUT = {
+                active: true,
+                name: "Inserted User",
+                password: "insertedpassword",
+                scope: "superuser",
+                username: "inserted",
+            };
+
+            const OUTPUT = await UserServices.insert(INPUT);
+            compareUserNew(OUTPUT, INPUT);
+
+            const FOUND = await UserServices.find(OUTPUT.id);
+            compareUserOld(FOUND, OUTPUT);
+
+        })
+
     })
 
     describe("UserServices.refreshTokens()", () => {
@@ -224,11 +390,129 @@ describe("UserServices Functional Tests", () => {
     })
 
     describe("UserServices.remove()", () => {
-        // TODO
+
+        it("should fail on invalid ID", async () => {
+
+            const INVALID_ID = -1;
+
+            try {
+                await UserServices.remove(INVALID_ID);
+                expect.fail("Should have thrown NotFound");
+            } catch (error) {
+                if (error instanceof NotFound) {
+                    expect(error.message).to.include
+                    (`userId: Missing User ${INVALID_ID}`);
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+        })
+
+        it("should pass on valid input", async () => {
+
+            const INPUT = await lookupUser(SeedData.USER_USERNAME_SUPERUSER);
+            const OUTPUT = await UserServices.remove(INPUT.id);
+            expect(OUTPUT.id).to.equal(INPUT.id);
+
+            try {
+                await UserServices.remove(INPUT.id);
+                expect.fail(`Should have thrown NotFound after remove`);
+            } catch (error) {
+                if (error instanceof NotFound) {
+                    expect(error.message).to.include(`userId: Missing User ${INPUT.id}`);
+                } else {
+                    expect.fail(`Should have thrown NotFound`);
+                }
+            }
+
+        })
+
     })
 
     describe("UserServices.update()", () => {
-        // TODO
+        it("should fail on duplicate username", async () => {
+
+            const ORIGINAL = await lookupUser(SeedData.USER_USERNAME_SUPERUSER);
+            const INPUT = {
+                active: ORIGINAL.active,
+                name: ORIGINAL.name,
+                scope: ORIGINAL.scope,
+                username: SeedData.USER_USERNAME_FIRST_ADMIN,
+            }
+
+            try {
+                await UserServices.update(ORIGINAL.id, INPUT);
+                expect.fail("Should have thrown BadRequest");
+            } catch (error) {
+                if (error instanceof BadRequest) {
+                    expect(error.message).to.include
+                    (`username: Username '${INPUT.username}' is already in use`);
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should fail on invalid ID", async () => {
+
+            const INVALID_ID = -1;
+            const ORIGINAL = await lookupUser(SeedData.USER_USERNAME_SUPERUSER);
+            const INPUT = {
+                active: ORIGINAL.active,
+                name: ORIGINAL.name,
+            }
+
+            try {
+                await UserServices.update(INVALID_ID, INPUT);
+                expect.fail("Should have thrown NotFound");
+            } catch (error) {
+                if (error instanceof NotFound) {
+                    expect(error.message).to.include
+                    (`userId: Missing User ${INVALID_ID}`);
+                } else {
+                    expect.fail(`Should not have thrown '${error}'`);
+                }
+            }
+
+        })
+
+        it("should pass on on changes data", async () => {
+
+            const INPUT = await lookupUser(SeedData.USER_USERNAME_FIRST_REGULAR);
+
+            const OUTPUT = await UserServices.update(INPUT.id, INPUT);
+            compareUserOld(OUTPUT, INPUT);
+
+        })
+
+        it("should pass on no updates data", async () => {
+
+            const ORIGINAL = await lookupUser(SeedData.USER_USERNAME_SECOND_REGULAR);
+            const INPUT: Partial<User> = {};
+
+            const OUTPUT = await UserServices.update(ORIGINAL.id, INPUT);
+            compareUserOld(OUTPUT, INPUT);
+            const UPDATED = await UserServices.find(ORIGINAL.id);
+            compareUserOld(UPDATED, OUTPUT);
+
+        })
+
+        it("should pass on valid updates data", async () => {
+
+            const ORIGINAL = await lookupUser(SeedData.USER_USERNAME_SECOND_ADMIN);
+            const INPUT = {
+                active: !ORIGINAL.active,
+                scope: ORIGINAL.scope + " extra",
+            }
+
+            const OUTPUT = await UserServices.update(ORIGINAL.id, INPUT);
+            compareUserOld(OUTPUT, INPUT);
+            const UPDATED = await UserServices.find(ORIGINAL.id); // Pick up password redact
+            compareUserOld(UPDATED, OUTPUT);
+
+        })
+
     })
 
 })
