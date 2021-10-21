9a6c3128071057608b8cb3fcbf3bf8da6cc5467c
Craig McClanahan <craigmcc@gmail.com>
2021-10-20 19:49:48 -0700

    Support HTTP and HTTPS connections (need certificates for HTTPS). Use PORT and PORT_HTTPS environment variables for port numbers to be enabled.

--- src/server.ts
+++ src/server.ts
@@ -4,9 +4,14 @@
 
 // External Modules ----------------------------------------------------------
 
+import * as fs from "fs";
+
 require("custom-env").env(true);
+const https = require("https");
+
 export const NODE_ENV = process.env.NODE_ENV ? process.env.NODE_ENV : "production";
-export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 8081;
+export const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : undefined;
+export const PORT_HTTPS = process.env.PORT_HTTPS ? parseInt(process.env.PORT_HTTPS, 10) : undefined;
 
 // Internal Modules ----------------------------------------------------------
 
@@ -20,11 +25,27 @@ Database.getDatabaseName(); // Trigger initialization of Database module
 
 // Configure and Start Server ------------------------------------------------
 
-ExpressApplication.listen(PORT, () => {
-    logger.info({
-        context: "Startup",
-        msg: "Start HTTP Server",
-        mode: NODE_ENV,
-        port: PORT,
+if (PORT) {
+    ExpressApplication.listen(PORT, () => {
+        logger.info({
+            context: "Startup",
+            msg: "Start HTTP Server",
+            mode: NODE_ENV,
+            port: PORT,
+        });
     });
-});
+}
+
+if (PORT_HTTPS && process.env.HTTPS_CERT && process.env.HTTPS_KEY) {
+    https.createServer({
+        cert: fs.readFileSync(process.env.HTTPS_CERT),
+        key: fs.readFileSync(process.env.HTTPS_KEY),
+    }, ExpressApplication).listen(PORT_HTTPS, () => {
+        logger.info({
+            context: "Startup",
+            msg: "Start HTTPS Server",
+            mode: NODE_ENV,
+            port: PORT_HTTPS,
+        });
+    });
+}
