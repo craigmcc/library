eefbb8c7ab9c27c133703b9cd5dcdb0432dd6c98
Craig McClanahan <craigmcc@gmail.com>
2021-11-05 11:23:17 -0700

    Tweak navbar styling to at least get full width.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -24,6 +24,7 @@ import SeriesView from "./components/series/SeriesView";
 import StoriesView from "./components/stories/StoriesView";
 import UsersView from "./components/users/UsersView";
 import VolumesView from "./components/volumes/VolumesView";
+import Container from "react-bootstrap/Container";
 
 // Component Details ---------------------------------------------------------
 
@@ -34,45 +35,40 @@ function App() {
 
         <Router>
 
-            <Navbar bg="light">
-                <Navbar
-                    bg="primary"
-                    className="mb-3"
-                    sticky="top"
-                    variant="dark"
-                >
-                    <Navbar.Brand className="ms-2" href="/">
-                        <img
-                            alt="Library Management"
-                            height={60}
-                            src="./books.jpeg"
-                            width={100}
-                        />
-                        <span className="ms-2">Library Management</span>
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                    <Navbar.Collapse id="basic-navbar-nav">
-                        <Nav className="me-auto">
-                            <Nav.Link href="/">Home</Nav.Link>
-                            <Nav.Link href="/authors">Authors</Nav.Link>
-                            <Nav.Link href="/series">Series</Nav.Link>
-                            <Nav.Link href="/stories">Stories</Nav.Link>
-                            <Nav.Link href="/volumes">Volumes</Nav.Link>
-                            <NavDropdown title="Admin" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="/libraries">Libraries</NavDropdown.Item>
-                                <NavDropdown.Item href="/users">Users</NavDropdown.Item>
-                            </NavDropdown>
-                        </Nav>
-                    </Navbar.Collapse>
-                    <Nav>
-                        <span className="me-4"/>
-                        <LoggedInUser/>
-                        <span className="me-4"/>
-                        <LibrarySelector/>
-                        <span className="me-2"/>
-                    </Nav>
+            <Container className="mb-3 p-0" fluid>
+                <Navbar bg="primary" variant="dark">
+                    <Navbar sticky="top">
+                        <Navbar.Brand className="ms-2" href="/">
+                            <img
+                                alt="Library Management"
+                                height={60}
+                                src="./books.jpeg"
+                                width={100}
+                            />
+                            <span className="ms-2">Library Management</span>
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <Nav.Link href="/">Home</Nav.Link>
+                                <Nav.Link href="/authors">Authors</Nav.Link>
+                                <Nav.Link href="/series">Series</Nav.Link>
+                                <Nav.Link href="/stories">Stories</Nav.Link>
+                                <Nav.Link href="/volumes">Volumes</Nav.Link>
+                                <NavDropdown title="Admin" id="basic-nav-dropdown">
+                                    <NavDropdown.Item href="/libraries">Libraries</NavDropdown.Item>
+                                    <NavDropdown.Item href="/users">Users</NavDropdown.Item>
+                                </NavDropdown>
+                            </Nav>
+                        </Navbar.Collapse>
+                        <Container>
+                            <LoggedInUser/>
+                            <span className="me-4"/>
+                            <LibrarySelector/>
+                        </Container>
+                    </Navbar>
                 </Navbar>
-            </Navbar>
+            </Container>
 
             <Switch>
                 <Route exact path="/authors"><AuthorsView/></Route>
